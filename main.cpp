ef953f529ed8d1ea8e5f7cfe8125d084097e22cc
tot6829 <117469584+tot6829@users.noreply.github.com>
2022-11-09 00:58:04 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,35 @@
-// write your code here
+//Tatiyana Moore
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+cout << "Please enter the values of a, b, and c respectively" <<
+	endl;
+cin >> a >> b >> c;
+
+discriminant = (pow(b,2)) - (4*a*c);
+
+if (a == 0)
+{
+	cout << "Invalid Entry" << endl;
+}
+else if (discriminant == 0)
+{
+	x1 = (-b) / (2 * a);
+	cout << "The single (repeated) root is " << x1 << endl;
+}
+else if (discriminant > 0)
+{
+	firstRoot = (-b + sqrt(discriminant)) / (2 * a);
+	secondRoot = (-b - sqrt(discriminant)) / (2 * a);
+	cout << "Two real roots" << endl;
+	cout << "First root " << firstRoot << endl;
+	cout << "Second root " << secondRoot << endl;
+}
+else if (discriminant < 0)
+{
+	cout << "Complex roots " << endl;
+}
+return 0;
